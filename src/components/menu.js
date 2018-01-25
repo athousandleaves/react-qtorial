cc6291d811e357dee14678c025e842e1ffbae122
athousandleaves <superamare@gmail.com>
2018-01-25 10:35:38 -0600

    completely remove semantic UI dependency

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,30 +4,7 @@ export default class Header extends Component {
   render() {
     if (this.props.authed === true) {
       return (
-        <Menu inverted>
-          <Container>
-            <Menu.Item header>
-              <h1>Qtorial</h1>
-            </Menu.Item>
-            <Menu.Menu position="right">
-              <Menu.Item>
-                <Button secondary size="huge">
-                  Post New Tutorial
-                </Button>
-              </Menu.Item>
-              <Menu.Item>
-                <Button secondary size="huge">
-                  Logged in as currentUser.username
-                </Button>
-              </Menu.Item>
-              <Menu.Item>
-                <Button secondary size="huge">
-                  Logout
-                </Button>
-              </Menu.Item>
-            </Menu.Menu>
-          </Container>
-        </Menu>
+      <div></div>
       );
     } else {
       // not authenticated
