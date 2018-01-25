c3ce5aa6d20e5acfb07f3fd065b6aad9e3258066
athousandleaves <superamare@gmail.com>
2018-01-25 10:31:21 -0600

    remove semantic UI features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Menu from './components/menu';
+import Splash from './components/splash';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Menu />
+        <Splash />
       </div>
     );
   }
---
+++ src/components/splash.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class Splash extends Component {
+  render() {
+    const topics = [
+      {name: 'MongoDB'},
+      {name: 'JavaScript'},
+      {name: 'React'},
+      {name: 'Redux'}
+    ]
+
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
