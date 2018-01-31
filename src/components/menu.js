989e6daea6fe90a484e1fed650fd42e94772069e
athousandleaves <superamare@gmail.com>
2018-01-31 08:48:24 -0600

    add username to authed menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 
 export default class Header extends Component {
   render() {
-    if (this.props.authed === true) {
+    if (this.props.authed) {
       return (
       <div className="header">
-
+        <li>Post New Tutorial</li>
+        <li>Logged in as {this.props.username}</li>
+        <li>Logout</li>
       </div>
       );
     } else {
