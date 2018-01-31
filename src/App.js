97e6316196a2c456b19c54385b48b0c68bb83437
athousandleaves <superamare@gmail.com>
2018-01-30 19:33:58 -0600

    add recentlyAdded component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Menu from './components/menu';
 import Splash from './components/splash';
+import RecentlyAdded from './components/recentlyadded';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Menu />
         <Splash />
+        <RecentlyAdded />
       </div>
     );
   }
---
+++ src/components/recentlyadded.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class RecentlyAdded extends Component {
+  render() {
+    return (
+      <div className="recently-added">
+        <h1>Recently Added</h1>
+      </div>
+    )
+  }
+}
