ab6dc7e83d3d82b6df5d8ae3b1b39e0cc71479b4
athousandleaves <superamare@gmail.com>
2018-02-11 18:18:08 -0600

    add server routes for tutorial id

--- server/routes/tutorials.js
+++ server/routes/tutorials.js
@@ -1,48 +1,63 @@
-const router    = require('express').Router(),
-      topics    = require('../models/topics.js'),
-      tutorials = require('../models/tutorials.js');
+const router = require('express').Router(),
+  topics = require('../models/topics.js'),
+  tutorials = require('../models/tutorials.js');
+  comments = require('../models/comments.js');
 
-  router.get("/topics", function (req, res) {
-    topics.find({}, function(err, allTopics) {
-      if(err) {
+router.get("/topics", function (req, res) {
+  topics.find({}, function (err, allTopics) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(res.json(allTopics));
+    }
+  })
+});
+
+router.get("/tutorials", function (req, res) {
+  tutorials.find({}, function (err, allTutorials) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(res.json(allTutorials));
+    }
+  })
+});
+
+router.get("/topics/:id", function (req, res) {
+  // find topic and populate tutorials associated with it
+  topics
+    .findById(req.params.id)
+    .populate("tutorials")
+    .exec(function (err, foundTopic) {
+      if (err) {
         console.log(err);
       } else {
-        console.log(res.json(allTopics));
+        tutorials.find({ topic: foundTopic.name }, function (err, alltutorials) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(res.json({
+              topic: foundTopic,
+              tutorials: alltutorials
+            }));
+          }
+        });
       }
-    })
-  });
+    });
+});
 
-  router.get("/tutorials", function (req, res) {
-    tutorials.find({}, function(err, allTutorials) {
-      if(err) {
+// SHOW - shows more info about one tutorial
+router.get("/tutorials/:id", function (req, res) {
+  tutorials
+    .findById(req.params.id)
+    .populate("comments")
+    .exec(function (err, foundTutorial) {
+      if (err) {
         console.log(err);
       } else {
-        console.log(res.json(allTutorials));
+        console.log(res.json(foundTutorial));
       }
-    })
-  });
+    });
+});
 
-  router.get("/topics/:id", function(req, res) {
-    // find topic and populate tutorials associated with it
-    topics
-      .findById(req.params.id)
-      .populate("tutorials")
-      .exec(function(err, foundTopic) {
-        if (err) {
-          console.log(err);
-        } else {
-          tutorials.find({ topic: foundTopic.name }, function(err, alltutorials) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log(res.json({
-                topic: foundTopic,
-                tutorials: alltutorials
-              }));
-            }
-          });
-        }
-      });
-  });
-
-  module.exports = router;
+module.exports = router;
